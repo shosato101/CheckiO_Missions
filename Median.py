f5e15fb093fc92b28bdbe2147a837d98e0d911e1
s.sato <beetbood@gmail.com>
2021-01-11 21:35:04 +0900

    apply pipenv

--- Median.py
+++ Median.py
@@ -45,4 +45,4 @@ if __name__ == '__main__':
     assert get_median([1, 300, 2, 200, 1]) == "2", "It's not an average"
     assert get_median([3, 6, 20, 99, 10, 15]) == "12.5", "Even length"
     assert get_median(list(range(1000000))) == "499999.5", "Long."
-    print("Coding complete!")
+    print("Cording complete!")
